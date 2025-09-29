7868d1adcfbef8a36921c02a8540ca3804072fb4
Zilan Hasnath Lithon <zilanhasnath@gmail.com>
2025-09-29 23:20:36 +0600

    Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import Navbar from '../Components/Navbar'
 import Main from '../Components/Main'
 
 const Home = () => {
     return (
         <>
             <div>
-                <Navbar />
                 <Main />
             </div>
         </>
